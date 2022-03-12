7ad6676b264bced2e799bf133b15d557ee474087
agsjorda <agsjorda@gmail.com>
2022-03-12 20:40:37 +0800

    updating app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React from "react";
-import "./App.css";
-import { Route, Switch } from "react-router-dom";
-import Homepage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import React from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import './homepage.styles.scss';
+import style from './homepage.module.scss';
 import Directory from '../../components/directory/directory.component';
 
 const Homepage = () => {
-  return (<div className='homepage'>
-      <Directory/>
-  </div>)
+  return (
+    <div className={style.homepage}>
+      <Directory />
+      <button className={style.btn}>Press me</button>
+    </div>
+  );
 };
 
 export default Homepage;
