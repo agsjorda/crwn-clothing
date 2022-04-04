1e0b0ef676eec5328187cccebf0cbe4a70f2976f
Arthur <agsjorda@gmail.com>
2022-04-04 21:32:31 +0800

    added signin component and firebase utils as well as firebase libraries

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,7 +6,7 @@ import {
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    const userDocRef = await sgicreateUserDocumentFromAuth(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
   return (
     <div>
